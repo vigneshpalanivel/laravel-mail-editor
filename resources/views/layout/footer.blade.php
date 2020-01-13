ceafdf68638cf21c2161cc9c1bc727337cd6af2c
vigneshpalanivel <pvignesh90@gmail.com>
2020-01-13 18:05:32 +0530

    footer design changes - Thangamani

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,10 +1,16 @@
-<footer class="text-right mt-4" style="font-size: 14px;">
-     {{-- <div class="text-muted">
+{{--<footer class="text-right mt-4" style="font-size: 14px;">
+      <div class="text-muted">
           <p>&copy; 2019 - <a href="https://maileclipse.io/" target="_blank">MailEclipse.io</a> v1.3</p>
 			<a class="github-button" href="https://github.com/qoraiche/laravel-mail-editor/issues" data-icon="octicon-issue-opened" aria-label="Issue qoraiche/laravel-mail-editor on GitHub">Issue</a>
 
 			<a class="github-button" href="https://github.com/qoraiche/laravel-mail-editor" data-icon="octicon-star" data-show-count="true" aria-label="Star qoraiche/laravel-mail-editor on GitHub">Star</a>
 
-      </div> --}}     
+      </div>    
       
- </footer>
+ </footer>--}}  
+ <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    </div>
+    <strong>Copyright &copy; 2017 <a href="">Trioangle Technologies</a>.</strong> All rights
+    reserved.
+</footer>
